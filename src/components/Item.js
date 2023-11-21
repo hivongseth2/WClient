e83ce620c72f0c40f30b83a9409691d19501e676
ChiThanh <thanh89508@gmail.com>
2023-11-21 18:36:03 +0700

    pull master

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,8 +9,13 @@ import BtnCart from "./BtnCart";
 const Item = (props) => {
   const [data, setData] = useState(props);
   const [img, setImg] = useState();
+  const [selectedHeart, setSelectedHeart] = useState(false);
   console.log(data.children.images[0]);
   console.log(data.children.productId);
+
+  const handleHeart = () => {
+    setSelectedHeart(!selectedHeart);
+  };
 
   const history = useHistory();
   // const url = "";
@@ -161,6 +166,49 @@ const Item = (props) => {
                 add to cart
               </button>
             </div>
+            <div
+              class="d-grid gap-2 my-4"
+              style={{ flexDirection: "row", justifyContent: "center" }}
+            >
+              <button>
+                <div class="default-btn">
+                  <svg
+                    class="css-i6dzq1"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    fill="none"
+                    stroke-width="2"
+                    stroke="#FFF"
+                    height="20"
+                    width="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle r="3" cy="12" cx="12"></circle>
+                  </svg>
+                  <span>Quick View</span>
+                </div>
+                <div class="hover-btn">
+                  <svg
+                    class="css-i6dzq1"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    fill="none"
+                    stroke-width="2"
+                    stroke="#ffd300"
+                    height="20"
+                    width="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle r="1" cy="21" cx="9"></circle>
+                    <circle r="1" cy="21" cx="20"></circle>
+                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                  </svg>
+                  <span>Shop Now</span>
+                </div>
+              </button>
+            </div>
+
             <div class="clearfix mb-1">
               <span class="float-start">
                 <a href="#">
@@ -168,8 +216,14 @@ const Item = (props) => {
                 </a>
               </span>
 
-              <span class="float-end">
-                <i class="far fa-heart" style={{ cursor: "pointer" }}></i>
+              <span className="float-end" onClick={handleHeart}>
+                <i
+                  className={selectedHeart ? "fas fa-heart" : "far fa-heart"}
+                  style={{
+                    cursor: "pointer",
+                    color: selectedHeart ? "red" : "black",
+                  }}
+                ></i>
               </span>
             </div>
           </div>
